e5b137e36bf6d4e80117b33436d1d8fa1678dc0d
centertong <centertong@gmail.com>
2021-02-24 15:43:55 +0000

    step04_part9_mnist_test

--- dezero/functions.py
+++ dezero/functions.py
@@ -229,7 +229,22 @@ class Sigmoid(Function):
 
 def sigmoid(x):
     return Sigmoid()(x)
+    
 
+class ReLU(Function):
+    def forward(self, x):
+        y = np.maximum(x, 0.0)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        mask = x.data > 0
+        gx = gy * mask
+        return gx
+
+
+def relu(x):
+    return ReLU()(x)
 
 class GetItem(Function):
     def __init__(self, slices):
--- tests/test_softmax.py
+++ tests/test_softmax.py
@@ -12,8 +12,8 @@ from dezero.datasets import Spiral
 
 
 class SoftmaxTest(unittest.TestCase):
-    def SoftmaxCrossEntorpy(self):
-        max_epoch = 1000
+    def test_SoftmaxCrossEntorpy(self):
+        max_epoch = 0
         batch_size = 30
         hidden_size = 10
         lr = 1.0
@@ -56,3 +56,46 @@ class SoftmaxTest(unittest.TestCase):
 
             print('test loss: {:.4f}, accuracy: {:.4f}'.format(
                 sum_loss / len(test_set), sum_acc / len(test_set)))
+
+    def test_MNIST(self):
+        max_epoch = 5
+        batch_size = 100
+        hidden_size = 1000
+
+        train_set = dezero.datasets.MNIST(train=True)
+        test_set = dezero.datasets.MNIST(train=False)
+        train_loader = DataLoader(train_set, batch_size)
+        test_loader = DataLoader(test_set, batch_size, shuffle=False)
+        
+        #model = MLP((hidden_size, 10))
+        model = MLP((hidden_size, hidden_size, 10), activation=F.relu)
+        optimizer = optimizers.SGD().setup(model)
+
+        for epoch in range(max_epoch):
+            sum_loss, sum_acc = 0, 0
+            for x, t in train_loader:
+                y = model(x)
+                loss = F.softmax_cross_entropy(y, t)
+                acc = F.accuracy(y, t)
+                model.cleargrads()
+                loss.backward()
+                optimizer.update()
+
+                sum_loss += float(loss.data) * len(t)
+                sum_acc += float(acc.data) * len(t)
+
+            print('epoch: {}'.format(epoch + 1))
+            print('train loss: {:.4f}, accuracy: {:.4f}'.format(
+                sum_loss / len(train_set), sum_acc / len(train_set)))
+            
+            sum_loss, sum_acc = 0, 0
+            with dezero.no_grad():
+                for x, t in test_loader:
+                    y = model(x)
+                    loss = F.softmax_cross_entropy(y, t)
+                    acc = F.accuracy(y, t)
+                    sum_loss += float(loss.data) * len(t)
+                    sum_acc += float(acc.data) * len(t)
+                
+                print('test loss: {:.4f}, accuracy: {:.4f}'.format(
+                    sum_loss / len(test_set), sum_acc / len(test_set)))
