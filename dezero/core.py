a56ff27d67dd7c2fea3a0a19cb7cdbd00edefe7d
centertong <centertong@gmail.com>
2021-02-25 06:06:28 +0000

    step05_part3_dropout

--- dezero/core.py
+++ dezero/core.py
@@ -167,6 +167,7 @@ class Function:
 
 class Config:
     enable_backprop = True
+    train = True
 
 @contextlib.contextmanager
 def using_config(name, value):
@@ -176,6 +177,9 @@ def using_config(name, value):
         yield
     finally:
         setattr(Config, name, old_value)
+
+def test_mode():
+    return using_config('train', False)
 
 def no_grad():
     return using_config('enable_backprop', False)
